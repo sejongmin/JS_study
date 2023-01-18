21382a18c6cb3acdb917bd239f562be5f3eda082
OhJongMin <jmoh7480@gmail.com>
2023-01-18 15:20:55 +0900

    Compare, if-else

---
+++ _기초_/3-4.js
@@ -0,0 +1,5 @@
+const a = 1;
+const b = "1";
+
+console.log(a == b);
+console.log(a === b);   //일치 연산자 type 까지 비교함
---
+++ _기초_/3-5.js
@@ -0,0 +1,9 @@
+const age = 19;
+
+if (age > 19){
+    console.log("환영합니다.");
+}else if (age === 19){
+    console.log("수능 잘치세요.");
+}else{
+    console.log("안녕히 가세요.");
+}
